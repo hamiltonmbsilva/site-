35d53065347164797fd2d9c8445e53ec1091bba0
Hamilton <hamiltonmb.silva@gmail.com>
2018-07-19 01:03:10 -0300

    atualização de contato, e portifolio

---
+++ processa.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+    <head>
+        <meta charset="UTF-8">
+        <title></title>
+    </head>
+    <body>
+        <?php
+		$nome = $_POST['nome'];
+		$email = $_POST['email'];
+		$telefone = $_POST['telefone'];
+		$assunto = $_POST['assunto'];
+		$mensagem = $_POST['mensagem'];
+		
+        require 'vendor/autoload.php';
+
+        $from = new SendGrid\Email(null, "hamilton_mb@hotmail");
+        $subject = "Mensagem de contato";
+        $to = new SendGrid\Email(null, "hamiltonmb.silva@gmail.com");
+        $content = new SendGrid\Content("text/html", "Olá Hamilton, <br><br>Nova mensagem de contato<br><br>
+		Nome: $nome<br>Email: $email <br>Telefone: $telefone <br>Assunto: $assunto <br>Mensagem: $mensagem");
+        $mail = new SendGrid\Mail($from, $subject, $to, $content);
+        
+        //Necessário inserir a chave
+        $apiKey = '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';
+        $sg = new \SendGrid($apiKey);
+
+        $response = $sg->client->mail()->send()->post($mail);
+        echo "Mensagem enviada com sucesso";
+		
+        ?>
+    </body>
+</html>
